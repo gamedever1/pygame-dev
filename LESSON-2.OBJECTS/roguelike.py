6487feac55d8c0622266b5392da119caf9699d7f
gamedever1 <55281328+gamedever1@users.noreply.github.com>
2019-09-13 17:07:25 +0500

    Add files via upload

---
+++ LESSON-2.OBJECTS/roguelike.py
@@ -0,0 +1,193 @@
+#/usr/bin/env python
+
+# TODO: Horizontal collision detection, everything else
+
+import os, pygame
+from pygame.locals import *
+from random import randint, choice, randrange ###
+from utils import loadSpritesheet, loadCharacterSpritesheet
+from models import Level, Tile, Entity, gameObj ###
+
+
+
+playerType = 'ranger'
+gameScale = 2
+tileData = [
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+        ]
+decorationData = [
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+        ]
+
+xRes, yRes = (int)(gameScale * 256), (int)(gameScale * 256)
+level = Level(16, 16, 2)
+level.setAllTiles(tileData)
+level.setAllDecorations(decorationData)
+
+def loadTextures():
+    global playerTextures, wallTextures, ceilingTextures, floorTextures
+
+    global gameObjectList #глобальная переменная со всеми объектами в игре 
+    gameObjectList = []#делаем переменную пустым списком
+    
+    itemSprites1 = loadSpritesheet('assets/Items1.png', 16, 32, xScale=gameScale, yScale=gameScale) #режем Items.png на отдельные картинки-объекты
+    for i in range(len(itemSprites1)): #для каждой нарезанной картинки-объекта, помещаем его в случайное место на карте
+        gameObjectList.append(gameObj([randrange(0, xRes-itemSprites1[i].get_rect().w),randrange(0, yRes - itemSprites1[i].get_rect().h)], itemSprites1[i], 'item' + str(i))) 
+    treeSprites = loadSpritesheet('assets/trees1.png', 114, 100, xScale=gameScale, yScale=gameScale) #режем Items.png на отдельные картинки-объекты
+    for i in range(len(treeSprites)): #деревья также разбрасываем в случайные места на карте
+        gameObjectList.append(gameObj([randrange(0, xRes-treeSprites[i].get_rect().w),randrange(0, yRes - treeSprites[i].get_rect().h)], treeSprites[i], 'a tree ' + str(i)))
+    gameObjectList.sort(key=lambda x: x.pos[1]+x.lowestY) #сортируем массив объектов по ключу самого 
+    
+    
+    playerTextures = loadCharacterSpritesheet('assets/' + playerType + '.png', 32, 32, 10, 10, xScale=gameScale, yScale=gameScale, colorkey=-1)
+    wallTextures = loadSpritesheet('assets/wall.png', 16, 32, xScale=gameScale, yScale=gameScale)
+    ceilingTextures = loadSpritesheet('assets/ceiling.png', 16, 16, xScale=gameScale, yScale=gameScale)
+    floorTextures = loadSpritesheet('assets/floor.png', 16, 16, xScale=gameScale, yScale=gameScale)
+
+
+
+###    
+
+def loadLevel(tileData, decorationData, outputLevel):
+    for y in range(0, 16):
+        for x in range(0, 16):
+            if tileData[y][x] == 'W':
+                tile = Tile('wall', False)
+                tile.image = choice(wallTextures)
+                tile.offsetY = -16 * gameScale
+            elif tileData[y][x] == 'c':
+                tile = Tile('ceiling', True)
+                tile.image = choice(ceilingTextures)
+                tile.offsetY = 0
+            else:
+                tile = Tile('floor', True)
+                tile.image = choice(floorTextures)
+                tile.offsetY = 0
+            if decorationData[y][x] == 'c':
+                dec = Tile('ceiling', True)
+                dec.image = choice(ceilingTextures)
+                dec.offsetY = -16 * 2 * gameScale
+            else:
+                dec = None
+            outputLevel.setTileAt(x, y, tile)
+            outputLevel.setDecorationAt(x, y, dec)
+            
+    
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((xRes, yRes))
+    pygame.display.set_caption('Roguelike')
+
+    animationIndex, spriteIndex = 0, 0
+    clock = pygame.time.Clock()
+    player = Entity(level)
+
+    loadTextures()
+    loadLevel(tileData, decorationData, level)
+
+    while True:
+        clock.tick(24)
+
+        player.isMoving = False
+        player.dx, player.dy = 0, 0
+
+        keys = pygame.key.get_pressed()
+        if keys[K_ESCAPE]: return
+        if keys[K_UP]:
+            player.isMoving = True
+            player.dy = -2 * gameScale
+        if keys[K_DOWN]:
+            player.isMoving = True
+            player.dy = 2 * gameScale
+        if keys[K_LEFT]:
+            player.isFacingRight = False
+            player.isMoving = True
+            player.dx = -2 * gameScale
+        if keys[K_RIGHT]:
+            player.isFacingRight = True
+            player.isMoving = True
+            player.dx = 2 * gameScale
+        if keys[K_z]:
+            player.isCasting = True
+            animationIndex = 0
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return
+
+        if animationIndex < 9:
+            animationIndex += 1
+        else:
+            animationIndex = 0
+            player.isCasting = False
+            player.isMoving = False
+
+        if player.isCasting:
+            spriteIndex = 3
+        elif player.isMoving:
+            spriteIndex = 2
+        else:
+            spriteIndex = 0
+
+        player.image, player.rect = playerTextures[spriteIndex][animationIndex]
+        if not player.isFacingRight:
+            player.image = pygame.transform.flip(player.image, True, False)
+        
+        pX, pY = player.update()
+        for y in range(0, 16):
+            for x in range(0, 16):
+                screen.blit(level.getTileAt(x, y).image, (x * 16 * gameScale, y * 16 * gameScale + level.getTileAt(x, y).offsetY))
+                if (level.getDecorationAt(x, y) is not None):
+                    screen.blit(level.getDecorationAt(x, y).image, (x * 16 * gameScale, y * 16 * gameScale + level.getDecorationAt(x, y).offsetY))
+                if ((x, y - 1) == (pX, pY)):
+                    screen.blit(player.image, (player.x + player.offsetX, player.y + player.offsetY))
+
+    #####################
+        playerPos = (player.x + player.offsetX, player.y + player.offsetY) #позиция игрока
+        #screen.blit(player.image, playerPos)
+        for indx,_ in enumerate(gameObjectList):
+            curObj = gameObjectList[indx]
+            #pygame.draw.rect(screen, (255,255,255), Rect(curObj.pos[0] + curObj.opaqRect[0], curObj.pos[1] + curObj.opaqRect[1], curObj.opaqRect[2], curObj.opaqRect[3]), 2)
+            screen.blit(curObj.img, curObj.pos) ###
+            #pygame.draw.rect(screen, (255,0,0), Rect(curObj.pos[0] + curObj.colRect[0], curObj.pos[1] + curObj.colRect[1], curObj.colRect[2], curObj.colRect[3]), 5)
+            if (playerPos[1] + player.rect[3]) >= (curObj.pos[1] + curObj.lowestY):
+                screen.blit(player.image, playerPos)
+        ##pygame.draw.rect(screen, (255,255,255,5), Rect(playerPos[0], playerPos[1], player.rect[2], player.rect[3]), 4)
+
+    ##################### 
+
+
+        pygame.display.flip()
+
+if __name__ == '__main__': main()
